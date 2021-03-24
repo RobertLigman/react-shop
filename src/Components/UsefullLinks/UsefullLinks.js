73011934675605dec8382dcf79b335c194d31e8f
Robert Ligman <verxus123@gmail.com>
2021-03-24 18:04:09 +0100

    add font awesome and proper link names to usefulllinks component

--- src/Components/UsefullLinks/UsefullLinks.js
+++ src/Components/UsefullLinks/UsefullLinks.js
@@ -1,28 +1,20 @@
+import {
+  faFacebookF,
+  faInstagram,
+  faPinterest,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 import "./UsefullLinks.css";
-const information = [
-  {
-    name: "The Brand",
-    link: "brand",
-  },
-  {
-    name: "Local Stores",
-    link: "localstores",
-  },
-  {
-    name: "Customer service",
-    link: "customerservice",
-  },
-  {
-    name: "Privacy & cookies",
-    link: "privacy&coockies",
-  },
-  {
-    name: "Site Map",
-    link: "sitemap",
-  },
-];
+import {
+  information,
+  lookbook,
+  yourAccount,
+  whyBuy,
+  contactDetails,
+} from "./UsefullLinksAssets";
 function UsefullLinks() {
   return (
     <div className="UsefullLinks">
@@ -37,9 +29,10 @@ function UsefullLinks() {
             </li>
           ))}
         </ul>
+
         <ul className="UsefullLinks__list">
-          <p className="UsefullLinks__subtitle">information</p>
-          {information.map((el) => (
+          <p className="UsefullLinks__subtitle">why buy from us</p>
+          {whyBuy.map((el) => (
             <li className="UsefullLinks__item">
               <Link to={`/${el.link}`} className="UsefullLinks__link">
                 {el.name}
@@ -48,8 +41,8 @@ function UsefullLinks() {
           ))}
         </ul>
         <ul className="UsefullLinks__list">
-          <p className="UsefullLinks__subtitle">information</p>
-          {information.map((el) => (
+          <p className="UsefullLinks__subtitle">your Account</p>
+          {yourAccount.map((el) => (
             <li className="UsefullLinks__item">
               <Link to={`/${el.link}`} className="UsefullLinks__link">
                 {el.name}
@@ -58,8 +51,8 @@ function UsefullLinks() {
           ))}
         </ul>
         <ul className="UsefullLinks__list">
-          <p className="UsefullLinks__subtitle">information</p>
-          {information.map((el) => (
+          <p className="UsefullLinks__subtitle">lookbook</p>
+          {lookbook.map((el) => (
             <li className="UsefullLinks__item">
               <Link to={`/${el.link}`} className="UsefullLinks__link">
                 {el.name}
@@ -68,23 +61,31 @@ function UsefullLinks() {
           ))}
         </ul>
         <ul className="UsefullLinks__list">
-          <p className="UsefullLinks__subtitle">information</p>
-          {information.map((el) => (
+          <p className="UsefullLinks__subtitle">contact Details</p>
+          {contactDetails.map((el) => (
             <li className="UsefullLinks__item">
-              <Link to={`/${el.link}`} className="UsefullLinks__link">
-                {el.name}
-              </Link>
+              <p className="UsefullLinks__text">{el.name}</p>
             </li>
           ))}
         </ul>
         <div className="banner-section">
           <div className="banner banner--dark">
-            <h2>Award winner</h2>
-            <h2>Fashion awards 2016</h2>
+            <h2 className="banner__title">Award winner</h2>
+            <h2 className="banner__subtitle">Fashion awards 2016</h2>
           </div>
-          <div className="banner banner--light">
-            <h2>Award winner</h2>
-            <h2>Fashion awards 2016</h2>
+          <div className="banner banner--light grid-layout">
+            <a href="https://facebook.com" className="banner__link">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href="https://twitter.com" className="banner__link">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a href="https://instagram.com" className="banner__link">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a href="https://pinterest.com" className="banner__link">
+              <FontAwesomeIcon icon={faPinterest} />
+            </a>
           </div>
         </div>
       </div>
---
+++ src/Components/UsefullLinks/UsefullLinksAssets.js
@@ -0,0 +1,107 @@
+export const information = [
+  {
+    name: "The Brand",
+    link: "brand",
+  },
+  {
+    name: "Local Stores",
+    link: "localstores",
+  },
+  {
+    name: "Customer service",
+    link: "customerservice",
+  },
+  {
+    name: "Privacy & cookies",
+    link: "privacy&coockies",
+  },
+  {
+    name: "Site Map",
+    link: "sitemap",
+  },
+];
+export const whyBuy = [
+  {
+    name: "Shipping & returns",
+    link: "s&r",
+  },
+  {
+    name: "Secure shopping",
+    link: "secureshopping",
+  },
+  {
+    name: "Testimonials",
+    link: "testimonials",
+  },
+  {
+    name: "Award winning",
+    link: "award",
+  },
+  {
+    name: "Ethical trading",
+    link: "ethicaltrading",
+  },
+];
+export const yourAccount = [
+  {
+    name: "SignIn",
+    link: "signIn",
+  },
+  {
+    name: "Register",
+    link: "register",
+  },
+  {
+    name: "View cart",
+    link: "cart",
+  },
+  {
+    name: "View your lookbook",
+    link: "lookbook",
+  },
+  {
+    name: "Track an order",
+    link: "tracking",
+  },
+  {
+    name: "Update information",
+    link: "profile edit",
+  },
+];
+export const lookbook = [
+  {
+    name: "Latest posts",
+    link: "latest",
+  },
+  {
+    name: "Men's lookbook",
+    link: "mens lookbook",
+  },
+  {
+    name: "Women's lookbook",
+    link: "Womens lookbook",
+  },
+  {
+    name: "Lookbooks RSS feed",
+    link: "rss",
+  },
+  {
+    name: "View your lookbook",
+    link: "your lookbook",
+  },
+  {
+    name: "Delete your lookbook",
+    link: "delete your lookbook",
+  },
+];
+export const contactDetails = [
+  {
+    name: "Head Office: Avenue Fashion, 180-182 Regent Street, London",
+  },
+  {
+    name: "Telephone: 0123-456-789",
+  },
+  {
+    name: "Email: support@yourwebsite.com",
+  },
+];
