9e406f78314f44b315f1eec6c0cc5024a6d9b63d
Robert Ligman <verxus123@gmail.com>
2021-04-01 11:36:55 +0200

    add some styling for mobile hero component

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+function Products() {
+  const { name } = useParams();
+  return (
+    <div>
+      <h3>{name}</h3>
+    </div>
+  );
+}
+
+export default Products;
